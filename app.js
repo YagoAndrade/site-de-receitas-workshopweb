81b7821ca00358e074709209e5515c5091482bc1
Yago <afrtm61@outlook.com>
2020-12-14 18:37:26 -0300

    Rodapé adicionado + função com data em JS

---
+++ app.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+    var element = document.getElementById('container');
+    var height = element.offsetHeight;
+    if (height < screen.height) {
+        document.getElementById("footer").classList.add('sticktobottom');
+    }
+}, false);
